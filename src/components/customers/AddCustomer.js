17e18abe5cf68a1be16ccf0de90c179f61e0e3a6
domman95 <dominiklasek95@gmail.com>
2020-03-02 14:03:06 +0100

    fixed validation in AddCustomer

--- src/components/customers/AddCustomer.js
+++ src/components/customers/AddCustomer.js
@@ -26,11 +26,15 @@ class AddCustomer extends Component {
 
 
     handleChange = (e) => {
-        if (e.target.value.length === 3 && e.target.name === 'phoneNumber' && this.state.clickedKey !== 'Backspace') {
+        if ((e.target.value.length === 3 || e.target.value.length === 7) && e.target.name === 'phoneNumber' && this.state.clickedKey !== "Backspace") {
             e.target.value = e.target.value + '-'
-        } else if (e.target.value.length === 7 && e.target.name === 'phoneNumber' && this.state.clickedKey !== 'Backspace') {
-            e.target.value = e.target.value + '-'
+        } else if (e.target.value.length === 9 && e.target.name === 'phoneNumber' && !e.target.value.includes('-')) {
+            let modifiedPhoneNumber = e.target.value.split('')
+            modifiedPhoneNumber.splice(3, 0, '-')
+            modifiedPhoneNumber.splice(7, 0, '-')
+            e.target.value = modifiedPhoneNumber.join('')
         }
+
         this.setState({
             [e.target.name]: e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
         })
@@ -38,20 +42,17 @@ class AddCustomer extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-        if (this.state.firstName === '' || this.state.lastName === '' || this.state.phoneNumber.length !== 11) {
-            alert('Wypełnij wszystkie pola')
-        } else {
-            const form = document.querySelector('form')
-            this.props.createCustomer(this.state)
-            this.setState({
-                firstName: '',
-                lastName: '',
-                phoneNumber: ''
-            })
-            form.firstName.value = ''
-            form.lastName.value = ''
-            form.phoneNumber.value = ''
-        }
+        const form = document.querySelector('form')
+        form.firstName.value === '' || form.lastName.value === '' ? alert('Wypełnij wszystkie pola!') : this.props.createCustomer(this.state)
+        // this.props.createCustomer(this.state.users)
+        this.setState({
+            firstName: '',
+            lastName: '',
+            phoneNumber: ''
+        })
+        form.firstName.value = ''
+        form.lastName.value = ''
+        form.phoneNumber.value = ''
     }
     render() {
         return (
@@ -63,9 +64,9 @@ class AddCustomer extends Component {
                 </div>
                 <div className="addCustomerPanel">
                     <form onSubmit={this.handleSubmit}>
-                        <input autoComplete="off" type="text" name="firstName" onChange={this.handleChange} placeholder="Imię" />
-                        <input autoComplete="off" type="text" name="lastName" onChange={this.handleChange} placeholder="Nazwisko" />
-                        <input autoComplete="off" type="tel" name="phoneNumber" onChange={this.handleChange} pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" maxLength="11" placeholder="tel. 123-456-789" />
+                        <input autoComplete="off" type="text" name="firstName" onChange={this.handleChange} pattern="^[a-zA-Z]*" placeholder="Imię" />
+                        <input autoComplete="off" type="text" name="lastName" onChange={this.handleChange} pattern="^((?=[a-zA-Z,]*['.-][a-zA-Z,]*$)[a-zA-Z,'.-]+$|[a-zA-Z]*)" placeholder="Nazwisko" />
+                        <input autoComplete="off" type="tel" name="phoneNumber" onChange={this.handleChange} pattern="^[0-9]{3}-[0-9]{3}-[0-9]{3}" maxLength="11" placeholder="tel. 123-456-789" />
                         <input type="submit" className="btn" value="Dodaj" />
                     </form>
                 </div>
